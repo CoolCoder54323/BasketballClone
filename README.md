1257e342ce2f7f3a032bd224e6bbdb9bd0d03d00
nicholasdixon <nicxellent@icloud.com>
2023-12-25 13:46:19 -0500

    Removes players when they dont meet threshold

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-I want to be **Read**
+# NBA All Time At Least
+
+## Introduction
+**NBA All Time At Least** is an interactive website designed for basketball enthusiasts. It allows users to explore NBA player statistics based on specific performance metrics like Points Per Game (PPG), Assists Per Game (APG), and Total Rebounds (TRB).
+
+## Features
+- **Interactive Sliders:** Users can set minimum thresholds for PPG, APG, and TRB using sliders.
+- **Dynamic Player Display:** The website dynamically displays NBA players who meet the set criteria.
+- **Responsive Design:** Optimized for various devices and screen sizes.
+
+## Technologies Used
+- HTML/CSS/JavaScript
+- React.js
+- NBA Statistics API
+- Node.js for backend
+
+## Getting Started
+To set up this project locally:
+1. Clone the repo:
+
+2. Navigate to the project directory:
+
+3. Install dependencies:
+
+4. Start the server:
+
+5. Open `localhost:3000` in your browser.
+
+## Usage
+Adjust the PPG, APG, and TRB sliders to see a list of NBA players who have achieved these stats in a season.
+
+## Contributing
+Contributions are welcome! Please read the `CONTRIBUTING.md` file for details on our code of conduct and the process for submitting pull requests.
+
+## License
+This project is licensed under the MIT License - see the `LICENSE.md` file for details.
+
+## Acknowledgments
+- Thanks to the NBA Statistics API for providing comprehensive player data.
+- Shoutout to all contributors and basketball fans who provided valuable feedback.
--- script.js
+++ script.js
@@ -1,118 +1,147 @@
+
+var PPG = 0
+var APG = 0
+var TRB = 0
+var imageReady = false
+
+let hashMap = new Map();
+
+
+
 // JavaScript to update the label with the current value of the slider
-document.querySelectorAll('.slider').forEach(slider => {
-    slider.oninput = function() {
-        let valueId = this.id.split('-')[0] + '-value';
-        document.getElementById(valueId).textContent = this.value;
-    }
-    // Initialize text content
+function setSliders(slider) {
+    slider.oninput = updateSlider
     slider.oninput();
-});
+}
+
+function updateSlider(){
+        let id = this.id.split('-')[0]
+        let valueId = id + '-value';
+        switch(id)
+        {
+            case 'ppg':
+                PPG = this.value
+            case 'apg':
+                APG = this.value
+            case 'trb':
+                TRB = this.value
+        }
+        var removees = []
+        names = names.filter((name,index) => {
+            let playerData = hashMap.get(name);
+            console.log(playerData ,playerData[1], APG)
+            if (playerData && playerData[0] < PPG && playerData[1] < APG && playerData[2] < TRB){
+                widths.splice(index)
+                return false
+            }
+            return true
+        });
+        console.log(names)
+        addImages(names,widths)
+        
+
+        document.getElementById(valueId).textContent = this.value;
+}
+    
+
 
 function addImages(fnames,fwidths,heightMode="Max",height=-1,max=70) {
 
     console.log("Adding Images")
+    var container = document.querySelector(".images");
+    container.innerHTML = ''; // Clear existing content
     if(heightMode==="Max")
     {
-        var testarray = [0,2,3,4]
-        console.log(testarray)
-        testarray.forEach(value => {
-            console.log(value)
-        })
-        console.log("Max mode")
-        console.log(fnames)
-        console.log(fnames[1])
-        console.log(fnames)
-        let [firstFruit, secondFruit] = fnames;
-        console.log(firstFruit); // 'apple'
-        console.log(secondFruit); // 'banana'
-        for (let element of fnames) {
-            console.log(element);
+        if(height === -1){
+            fnames.forEach((item,index)=> {
+                addImage(item,fwidths[index],max)
+            })
         }
-        fnames.forEach((item, index) => {
-            console.log('Current item:', item);
-        });
-        fnames.forEach((item,index)=> {
-            console.log(item)
-            if(addImage(item,fwidths[index],max) != 0){
-                console.error("Image Adding Error")
-            }
-        })
-        console.log("All done :(")
-        console.log(fnames)
+        else if (height>0)
+        {
+            fnames.forEach((item,index)=> {
+                addImage(item,fwidths[index],height)
+            })
+        }
     }
     else if(heightMode === "fading-right")
     {
         if(height === -1)
         {
             fnames.forEach((item,index)=> {
-                if(addImage(item,fwidths[index],max*Math.pow(0.95,index))!==0){
-                    console.error("Image Adding Error")
-                }
+                addImage(item,fwidths[index],max*Math.pow(0.95,index))
                 
             }) 
         }
         else if(height > 0 )
         {
             fnames.forEach((item,index)=> {
-                if(addImage(item,fwidths[index],height*Math.pow(0.95,index))!==0){
-                    console.error("Image Adding Error")
-                }
+                addImage(item,fwidths[index],height*Math.pow(0.95,index))
             }) 
         }
     }
 }
 
-function addImage(fname, fwidth,height ) {
+function addImage(fname, fwidth,height ) 
+{
+    console.log(`Loading ${fname} at ${fwidth}vh long and ${height}vh tall`)
+
+
     // Create a new div element
     var newDiv = document.createElement("div");
-    height = Math.round(height)
-    console.log(`Loading ${fname} at ${fwidth}vh long and ${height}vh tall`)
+    var dynamicImage  = new Image()
     // Optionally, set some properties on the new div
     newDiv.className = "image-div";
-    let dynamicImage = document.createElement('img');
     dynamicImage.src = "PlayerImages/" + fname + ".png";
-    dynamicImage.style.height = `${height}vh`;
-    width = Math.round((60 * fwidth) * 10) / 10
+    dynamicImage.style.height = `${Math.round(height)}vh`;
+    width = Math.round((height * fwidth) * 10) / 10
     dynamicImage.style.width = `${width}vh`;
 
 
-    // Select the target element(s) where you want to add the new div
-    // This example selects all elements with the class name 'target-class'
-    var targetElements = document.querySelectorAll(".images");
+
+    var targetElement = document.querySelector(".images");
 
     newDiv.appendChild(dynamicImage);
 
 
     // Append the new div to each target element
-    targetElements.forEach(function(target) {
-        target.appendChild(newDiv.cloneNode(true)); // cloneNode is used to append the div to multiple targets
-    });
+    targetElement.append(newDiv.cloneNode(true))
 
 }
+
 var names = [""], widths = [""]
 var data
+function updateValues(json) {
+    data = json
+    names.length = json.length
+    widths.length = json.length
+    json.forEach((player,index)=>
+    {
+        if(hashMap.has(player.name)){
+            return
+        }
+        console.log(`APG: ${APG} ${player.name}s APG{player.apg}`)
+        if(player.ppg >= PPG && player.apg >= APG && player.trb >= TRB){
+            hashMap.set(player.name , [player.ppg,player.apg,player.trb])
+            names[index] = player.name
+            widths[index] = player.ratio
+        }
+
+        
+    })
+    addImages(names,widths,"Max",40)
+    imageReady = true
+}
+
 console.log(("Fetching"))
 fetch('./players.json')
     .then((response) => response.json())
     .then((json) => {
-        data = json
-        console.log("lofe")
-        if(console.log(json)){
-            console.log("What th")
-        }
-        
-
-        names.length = json.length
-        widths.length = json.length
-        json.forEach((player,index)=>
-        {
-            names[index] = player.name
-            widths[index] = player.ratio
-            
-        })
-        addImages(names,widths)
+        updateValues(json)
+        document.querySelectorAll('.slider').forEach(setSliders);
     }).catch(error => console.error('Error:', error));
-    console.log(data)
+
+
 
 
 
