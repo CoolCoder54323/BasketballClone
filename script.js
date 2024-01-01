31c625c612c4cb98d0333e6333120fc0b3560c97
nicholasdixon <nicxellent@icloud.com>
2024-01-01 03:34:39 -0500

    cleaned up javascript

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
 
-var PPG = 0
-var APG = 0
-var TRB = 0
-var IMAGEMODE = "Max"
+let PPG = 0
+let APG = 0
+let TRB = 0
+let IMAGEMODE = "Max"
 
 const MAX = 378
+const track = document.querySelector(".images");
+
 let offset;
 let nextPercentage;
 let mouseSelected = false
 
-// imageGrid = document.querySelector(".images-grid");
-
 let largestImage = ["",0]
 let highlightedPLayerDiv;
 
-var backgroundImage = document.querySelector('.content');
+let backgroundImage = document.querySelector('.content');
 let hashMap = new Map();
-const track = document.querySelector(".images");
-var textBoxes = document.querySelectorAll("#myTextBox")
-var cd = document.querySelector('.content');
-var width = cd.style.backgroundPositionX
+//let BGwidth = document.querySelector('.content').style.backgroundPositionX
 
 
 var names = [""]
-var data
 
 
 //Set listeners
@@ -42,32 +38,27 @@ window.onmouseup = (mouse) => {
 
     if(parseFloat(track.style.transform.split("(")[1])){
         currX = parseFloat(track.style.transform.split("(")[1])
-        newX = closestNumber(currX-offset,getWidth()[0]);
+        newX = closestNumber(currX-offset,getWidth());
     }
     
     track.animate({ transform: `translate(${newX + offset}px)`},{duration:500, fill:"forwards"})
     track.dataset.percentage = track.dataset.pastPercent = ((((newX)/track.clientWidth)*100))
-    highlightPlayer()
+    selectPlayer()
 
 }
 
-// Scroll content based on mouse movement
 window.onmousemove = e => {
 
-    // Check if mouse is down
     if(track.dataset.mouseDownAt==="0") return;
 
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-          maxDelta = window.innerWidth / 2;
-    const percentage = (mouseDelta/maxDelta) * -100
+          maxDelta = window.innerWidth / 2,
+          percentage = (mouseDelta/maxDelta) * -100
     moveImage(percentage)
-    highlightPlayer()
+    selectPlayer()
 
     // backgroundImage.animate({backgroundPositionX: `${-(nextPercentage)*50 + width}px`},{duration:500, fill:"forwards"})
-
 }
-
-
 
 document.addEventListener('keydown', (event) => {
     console.log(event.code)
@@ -118,52 +109,24 @@ document.addEventListener('keydown', (event) => {
 
 });
 
-function moveImage(percentage) {
-    offset = window.innerWidth/2 - track.clientWidth + getMiddle()
+/*
+        Set utility functions
+*/
 
-    nextPercentage = Math.min(Math.max(parseFloat(track.dataset.pastPercent) + percentage,0),90);
-    track.dataset.percentage = nextPercentage
-    track.animate({ transform: `translate(${(nextPercentage/100)*track.clientWidth+offset}px)`},{duration:percentage !== 0 ? 500 : 0, fill:"forwards"})
-    track.style.transform = `translate(${(nextPercentage/100)*track.clientWidth+offset}px)`
-    console.log("On mouse Move",track.style.transform)
-}
-    
-function closestNumber(n, m)
-{
- 
-    // find the quotient
+function closestNumber(n, m) {
+
     let q = parseInt(n / m);
-     
-    // 1st possible closest number
-    let n1 = m * q;
-     
-    // 2nd possible closest number
-    let n2 = (n * m) > 0 ?
+    let lowPos = m * q;
+    let highPos = (n * m) > 0 ?
         (m * (q + 1)) : (m * (q - 1));
+
+    //Return Closest    
+    if (Math.abs(n - lowPos) < Math.abs(n - highPos))
+        return lowPos;
      
-    // if true, then n1 is the
-    // required closest number
-    if (Math.abs(n - n1) < Math.abs(n - n2))
-        return n1;
-     
-    // else n2 is the required
-    // closest number
-    return n2;
+    return highPos;
 }
 
-function highlightPlayer() {
-    let pos = track.clientWidth * track.dataset.percentage/100,
-    playersMoved =  Math.round(pos/getWidth()[0])
-    console.log(`Highlighting the",${playersMoved} player`)
-    if(highlightedPLayerDiv == track.childNodes[track.childNodes.length-playersMoved-1]) return;
-
-    if(highlightedPLayerDiv) highlightedPLayerDiv.childNodes[0].classList.remove("selected-image-div");
-    highlightedPLayerDiv = track.childNodes[track.childNodes.length-playersMoved-1]
-    highlightedPLayerDiv.childNodes[0].classList.add("selected-image-div")
-    
-
-}
- 
 function roundTo(num, decimalPlaces) {
     var multiplier = Math.pow(10, decimalPlaces);
     return Math.round(num * multiplier) / multiplier;
@@ -173,35 +136,41 @@ function center() {
     let middle = window.innerWidth/2 - track.clientWidth
 }
 
-function removeImages(removeList,dur){
-    
-    multiplier = removeList.length
+const getWidth = ()=> { 
+    let imgArr = Array.from(document.querySelector(".images").childNodes)
 
-    removeList.forEach((name)=>{
-        let imageDiv = document.querySelector("." + name)
-        imageDiv.animate({width: "0px"},{duration:dur})
+    let [img] = imgArr.filter((element)=>{
+        element = (element).querySelector("*")
+        if(element.classList[0] === largestImage[0]){
+            return true
+        }
     })
-    setTimeout(()=>{addImages(names,heightMode=IMAGEMODE)}, dur)
 
+    img = img.querySelector("*")
+
+    return parseFloat(img.style.width.split('p')[0])
 }
 
-function addStylesheetRule(rule) {
-    var styleEl = document.createElement('style');
-    // Append <style> element to <head>
-    document.head.appendChild(styleEl);
+/*
+        Set utility functions
+*/
 
-    // Grab style sheet
-    var styleSheet = styleEl.sheet;
-    styleSheet.insertRule(rule);
+function selectPlayer() {
+    let pos = track.clientWidth * track.dataset.percentage/100,
+    playersMoved =  Math.round(pos/getWidth())
+    //console.log(`Highlighting the ${playersMoved} player`)
+    if(highlightedPLayerDiv == track.childNodes[track.childNodes.length-playersMoved-1]) return;
+
+    if(highlightedPLayerDiv) highlightedPLayerDiv.childNodes[0].classList.remove("selected-image-div");
+    highlightedPLayerDiv = track.childNodes[track.childNodes.length-playersMoved-1]
+    highlightedPLayerDiv.childNodes[0].classList.add("selected-image-div")
 }
 
-//Helper functions
 function setTextboxes(textbox) {
-    var id = textbox.id
     textbox.value = ""
     textbox.oninput = updateTextbox
     textbox.onclick = ()=>{
-        console.log("HELLO",textbox.valu,id)
+        console.log("HELLO",textbox.value,textbox.id)
 
         // if(textbox.value === ""){
         //     animateColor()
@@ -211,9 +180,9 @@ function setTextboxes(textbox) {
     textbox.oninput();
 }
 
-function updateTextbox() {
+function updateTextboxes() {
     let id = this.id.split('-')[0]
-    var value = this.value;
+    let value = this.value;
     this.value = value.replace(/[^0-9]/g, '');
 
     console.log(id)
@@ -270,92 +239,56 @@ function addImages(fnames,heightMode="Max",height=-1,max=MAX) {
         }
     }
 
-    // setGrid()
-    let [local_width, imgArr] = getWidth()
-    imgArr.forEach((elem,idx)=>{
-        elem.style.width = local_width + "px"
+    let local_width = getWidth()
+    Array.from(images.childNodes).forEach((image)=>{
+        image.style.width = local_width + "px"
     })
-}
-
-const getWidth = ()=> { 
-    let imgArr = Array.from(document.querySelector(".images"))
-    console.log(imgArr)
-
-    let [img] = imgArr.filter((element)=>{
-        element = Array.prototype.slice.call(element.children)[0]
-        if(element.classList[0] === largestImage[0]){
-            return true
-        }
-    })
-
-    img = img.querySelector("*")
-
-    return [parseFloat(img.style.width.split('p')[0]),imgArr]
 }
 
 function addImage(fname,height) {
-    var fratio = hashMap.get(fname)[3]
 
-    largestImage = fratio > largestImage[1] ? [fname,fratio] : largestImage
+    let fratio = hashMap.get(fname)[3]
+    let imageWidth = Math.round((height * fratio) * 10) / 10
+
     console.log(`Loading ${fname} at ${fratio}vh long and ${height}vh tall`)
     
 
-    // Create a new div element
-    var newDiv = document.createElement("div");
-    var dynamicImage  = new Image()
-    // Optionally, set some properties on the new div
-    newDiv.className = "image-div";
+    const dynamicImage  = new Image()
     dynamicImage.src = "PlayerImages/" + fname + ".png";
     dynamicImage.classList.add(fname)
     dynamicImage.style.height = `${Math.round(height)}px`;
-    width = Math.round((height * fratio) * 10) / 10
-    dynamicImage.style.width = `${width}px`;
+    dynamicImage.style.width = `${imageWidth}px`;
     dynamicImage.setAttribute("draggable","false")
 
-    var targetElement = document.querySelector(".images");
-
-    newDiv.addEventListener('mouseover',function(){
+    const newDiv = document.createElement("div");
+    newDiv.className = "image-div";
+    newDiv.appendChild(dynamicImage);
+    newDiv.addEventListener('mouseover',()=>{
         if(track.dataset.mouseDownAt !== "0") return;
         mouseSelected = true;
-        this.childNodes[0].classList.add("highlighted-image-div")
+        dynamicImage.classList.add("highlighted-image-div")
     })
-    newDiv.addEventListener("mouseleave",function(){
+    newDiv.addEventListener("mouseleave",()=>{
         if(track.dataset.mouseDownAt !== "0") return;
         mouseSelected = false;
-        this.childNodes[0].classList.remove("highlighted-image-div")
+        dynamicImage.classList.remove("highlighted-image-div")
     })
 
-    newDiv.appendChild(dynamicImage);
-
-    // Append the new div to each target element
-    targetElement.append(newDiv)
-
-
+    document.querySelector(".images").append(newDiv)
+    largestImage = fratio > largestImage[1] ? [fname,fratio] : largestImage
 }
 
-// function setGrid() {
-//     imageGrid.innerHTML = ""
-
-
-//     console.log("SETTING GRID")
-
-//     let maxWidth = document.querySelector(`.${largestImage[0]}`).clientWidth
-
-//     let tempCol = ""
-
-//     console.log(maxWidth)
+function removeImages(removeList,dur){
     
-//     for(let i = 0; i < names.length;i++){
-//         tempCol += `${maxWidth}px`
-//         if(i < Math.floor(imageGrid.clientWidth/maxWidth)-1) tempCol += " ";
-//         imageGrid.innerHTML += `<div>${i+1}</div>`
-//     }
-//     console.log(tempCol)
+    multiplier = removeList.length
 
-//     imageGrid.style.gridTemplateColumns = tempCol
+    removeList.forEach((name)=>{
+        let imageDiv = document.querySelector("." + name)
+        imageDiv.animate({width: "0px"},{duration:dur})
+    })
+    setTimeout(()=>{addImages(names,heightMode=IMAGEMODE)}, dur)
 
-
-// }
+}
 
 function initDisplay(json) {
     names.length = json.length
@@ -364,32 +297,34 @@ function initDisplay(json) {
         if(hashMap.has(player.name)){
             return
         }
-        console.log(`APG: ${APG} ${player.name}s APG{player.apg}`)
         if(player.ppg >= PPG && player.apg >= APG && player.trb >= TRB){
             hashMap.set(player.name , [player.ppg,player.apg,player.trb,player.ratio])
             names[index] = player.name
-            console.log(names)
         }
     })
     addImages(names,IMAGEMODE)
 
 }
 
-function getMiddle() {
-    return Number(getWidth()[0])/2
-}
+function moveImage(percentage) {
+    offset = window.innerWidth/2 - track.clientWidth + getWidth()/2
 
+    nextPercentage = Math.min(Math.max(parseFloat(track.dataset.pastPercent) + percentage,0),90);
+    track.dataset.percentage = nextPercentage
+    track.animate({ transform: `translate(${(nextPercentage/100)*track.clientWidth+offset}px)`},{duration:percentage !== 0 ? 500 : 0, fill:"forwards"})
+    track.style.transform = `translate(${(nextPercentage/100)*track.clientWidth+offset}px)`
+}
+    
 
 console.log(("Fetching"))
 fetch('./players.json')
     .then((response) => response.json())
     .then((json) => {
-        data = json
         initDisplay(json)
-        offset = window.innerWidth/2 - track.clientWidth + getMiddle();
-        console.log("Get width",(getWidth()[0]))
-        track.animate({ transform: `translate(${window.innerWidth/2 - track.clientWidth + getMiddle()}px)`},{duration:500, fill:"forwards"})
-        highlightPlayer()
+        offset = window.innerWidth/2 - track.clientWidth + getWidth()/2();
+        console.log("Width =",(getWidth()))
+        track.animate({ transform: `translate(${window.innerWidth/2 - track.clientWidth + getWidth()/2()}px)`},{duration:500, fill:"forwards"})
+        selectPlayer()
         console.log("Next Percentage",nextPercentage ? nextPercentage : 0)
         track.style.transform = `translate(${track.clientWidth + (offset)})px)`
         document.querySelectorAll('.textbox').forEach(setTextboxes);
